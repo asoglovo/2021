bae966964dceb435ec1b36a8b80b625d2d05c030
angel.sola <angel.sola@glovoapp.com>
2021-12-06 14:29:37 +0100

    Refactor: rename functions for clarity

--- 06/fish.py
+++ 06/fish.py
@@ -10,41 +10,40 @@ def total_fish_produced(fish_timer, simulation_duration):
     Computes the total number of fish produced by a fish with the given timer.
     This sum includes all generations and sub-generations of this fish.
     """
-    days = days_when_fish_produced(fish_timer, simulation_duration)
+    days = spawn_days_from_timer(fish_timer, simulation_duration)
     return len(days) + sum(
-        [total_produced_fish_spawning_at(day, simulation_duration)
+        [total_fish_produced_from_start_day(day, simulation_duration)
          for day in days]
     )
 
 
 @cache
-def total_produced_fish_spawning_at(day, simulation_duration):
+def total_fish_produced_from_start_day(day, simulation_duration):
     """
     Given the day where a new fish spawns and the simulation duration, computes
     the total number of produced fish by this fish and all its sub-generations.
     """
-    next_gen_days = spawn_days(day, simulation_duration)
+    next_gen_days = spawn_days_from_start_day(day, simulation_duration)
 
     return len(next_gen_days) + sum(
-        [total_produced_fish_spawning_at(day, simulation_duration)
+        [total_fish_produced_from_start_day(day, simulation_duration)
          for day in next_gen_days]
     )
 
 
-@cache
-def spawn_days(start_day, simulation_duration):
+def spawn_days_from_start_day(start_day, simulation_duration):
     """
     Given the day when a new fish has been spawned, it computes all the days
     where this fish would produce new fish in the simulation.
     """
-    return days_when_fish_produced(
+    return spawn_days_from_timer(
         spawned_fish_timer,
         simulation_duration,
         start_day
     )
 
 
-def days_when_fish_produced(fish_timer, simulation_duration, start_day=0):
+def spawn_days_from_timer(fish_timer, simulation_duration, start_day=0):
     """
     Given the initial timer of a fish, it returns a list of the simulation days
     where a new fish would be produced by this one.
--- 06/fish_test.py
+++ 06/fish_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from fish import days_when_fish_produced, spawn_days, total_fish_produced
+from fish import (spawn_days_from_start_day, spawn_days_from_timer,
+                  total_fish_produced)
 
 
 class FishTest(unittest.TestCase):
@@ -13,13 +14,22 @@ class FishTest(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    def test_days_when_fish_produced(self):
+    def test_spawn_days_from_timer(self):
         self.assertEqual(
             [4, 11, 18],
-            days_when_fish_produced(3, self.simulation_duration)
+            spawn_days_from_timer(3, self.simulation_duration)
         )
 
     def test_spawn_days(self):
-        self.assertEqual([13, 20], spawn_days(4, self.simulation_duration))
-        self.assertEqual([20], spawn_days(11, self.simulation_duration))
-        self.assertEqual([], spawn_days(18, self.simulation_duration))
+        self.assertEqual(
+            [13, 20],
+            spawn_days_from_start_day(4, self.simulation_duration)
+        )
+        self.assertEqual(
+            [20],
+            spawn_days_from_start_day(11, self.simulation_duration)
+        )
+        self.assertEqual(
+            [],
+            spawn_days_from_start_day(18, self.simulation_duration)
+        )
