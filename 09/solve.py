7a66f5600b1ba23c2c08e0919d791df9ae85438d
angel.sola <angel.sola@glovoapp.com>
2021-12-09 08:52:34 +0100

    Remove prints

--- 09/solve.py
+++ 09/solve.py
@@ -13,12 +13,6 @@ def read_heightmap():
 if __name__ == '__main__':
     heighmap = read_heightmap()
     lows = find_low_points(heighmap)
-
-    print(f'there are {len(lows)} low points')
-    for low in lows:
-        print(low)
-        assert all([low['height'] < side for side in low['sides']])
-
     total_risk = sum(
         [low['risk'] for low in lows]
     )
