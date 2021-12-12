7fc2224ea4b39e13e1b1441f4b3fcd7a72b1b0f0
angel.sola <angel.sola@glovoapp.com>
2021-12-12 11:15:39 +0100

    Day 12, part two

--- 12/solve.py
+++ 12/solve.py
@@ -6,13 +6,9 @@ def validate_next_node(node: Node, path: Path) -> bool:
     Checks whether the given next node can be added to the path.
 
     The rules are:
-        - If the node is a big cave, it can always be added to the path.
-        - If the node is a small cave it can be added if the cave is not 
-          yet in the path.
+    - If the node is a big cave, it can always be added to the path.
+    - If the node is a small cave it can be added if the cave is not yet in the path.
     """
-    if node.isupper():
-        return True
-
     small_caves_in_path = set([node for node in path if node.islower()])
     return node not in small_caves_in_path
 
--- 12/solve_p2.py
+++ 12/solve_p2.py
@@ -1,25 +1,34 @@
-from cavemap import find_all_paths, read_cave_map, Node, Path
+from collections import Counter
+
+from cavemap import Node, Path, find_all_paths, read_cave_map
 
 
 def validate_next_node(node: Node, path: Path) -> bool:
     """
     Checks whether the given next node can be added to the path.
-    The rules are:
 
+    The rules are:
     - If the node is a big cave, it can always be added to the path.
     - If the node is a small cave:
         - it can be added if the cave is not yet in the path
         - it can be added if the cave is already once in the path and no
           other small cave appears twice in the path.
     """
-    return True
+    if node.isupper():
+        return True
+
+    if node == 'start':
+        return False
+
+    small_caves_counter = Counter([node for node in path if node.islower()])
+    if node not in small_caves_counter:
+        return True
+
+    return small_caves_counter.most_common(1)[0][1] == 1
 
 
 if __name__ == '__main__':
     edges = read_cave_map()
-    paths = find_all_paths(edges)
-
-    # for path in paths:
-    #     print('-'.join(path))
+    paths = find_all_paths(edges, validate_next_node)
 
     print(f'There are {len(paths)} distinct paths.')
