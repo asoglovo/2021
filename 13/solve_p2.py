7c9827475236c4e283ca7e79ee487d1e56f02028
angel.sola <angel.sola@glovoapp.com>
2021-12-13 08:34:32 +0100

    Extract size variables

--- 13/solve_p2.py
+++ 13/solve_p2.py
@@ -1,9 +1,13 @@
 from origami import fold_along, print_dots, read_input
 
+width = 39
+height = 6
+
+
 if __name__ == '__main__':
     dots, instructions = read_input()
 
     for instruction in instructions:
         dots = fold_along(dots, instruction)
 
-    print_dots(dots, 50, 7)
+    print_dots(dots, width, height)
